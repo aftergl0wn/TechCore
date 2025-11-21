2bb6637bb301cd4cd0acd0cb5021b3c23fdf9ca9
Anastasia <aftergl0wn@yandex.ru>
2025-11-21 18:20:57 +0400

    Add list_student

---
+++ Module_1/list_student.py
@@ -0,0 +1,32 @@
+def filter_list(list_student):
+    new_list = []
+    for student in list_student:
+        if student["avg_grade"] > 4:
+            new_list.append(student)
+    return new_list
+
+
+if __name__ == "__main__":
+    list_student = [
+        {
+            "name": "Вася",
+            "avg_grade": 5,
+        },
+        {
+            "name": "Петя",
+            "avg_grade": 1.5,
+        },
+        {
+            "name": "Тася",
+            "avg_grade": 2.2,
+        },
+        {
+            "name": "Нина",
+            "avg_grade": 8,
+        },
+        {
+            "name": "Маша",
+            "avg_grade": 9,
+        },
+    ]
+    print(f"Список студентов {filter_list(list_student)}")
