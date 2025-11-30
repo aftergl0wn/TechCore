3f5435a6c0407540bfe48ece091f2b85821e07d0
Anastasia <aftergl0wn@yandex.ru>
2025-11-30 16:56:24 +0400

    Add wait_for

---
+++ Module_3/test_wait.py
@@ -0,0 +1,16 @@
+import logging
+import time
+
+import pytest
+
+from .wait import wait
+
+
+@pytest.mark.asyncio
+async def test_wait(caplog):
+    start = time.time()
+    with caplog.at_level(logging.ERROR):
+        await wait()
+    delta = time.time() - start
+    assert "Лимит времени исчерпан" in caplog.text
+    assert 1 < delta < 3
