379fb18c17988a0e31d248a02ad9b949d81960d1
Anastasia <aftergl0wn@yandex.ru>
2025-12-01 19:31:14 +0400

    Change pytest

--- Module_4/test_main.py
+++ Module_4/test_main.py
@@ -1,13 +1,16 @@
 from http import HTTPStatus
 
 from fastapi.testclient import TestClient
+import pytest
 
 from .main import app
 
-client = TestClient(app)
+@pytest.fixture
+def client():
+    return TestClient(app)
 
 
-def test_root():
+def test_root(client):
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == "Hello, World"
