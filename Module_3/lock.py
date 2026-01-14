71383a11f3bf42e3bda390b0dc6c26dad3add15e
Anastasia <aftergl0wn@yandex.ru>
2026-01-14 17:00:15 +0400

    Delete cache and url

--- Module_3/lock.py
+++ Module_3/lock.py
@@ -1,33 +1,16 @@
 import asyncio
-import logging
-
-import httpx
 
 
-async def response_url():
-    URL = "http://jsonplaceholder.typicode.com/posts/1"
-    async with httpx.AsyncClient() as client:
-        response = await client.get(URL)
-    return response
-
-
-async def get_info(coroutine, key, lock, cache, queue):
+async def get_info(lock):
     async with lock:
-        await queue.put(coroutine)
-        if key not in cache:
-            cache[key] = await response_url()
-        if queue.qsize() > 1:
-            logging.error("Обнаружено больше одной корутины")
-        await queue.get()
-    return cache[key]
+        await asyncio.sleep(1)
+    return "data"
 
 
 async def main():
-    cache = dict()
     lock = asyncio.Lock()
-    queue = asyncio.Queue()
     result = await asyncio.gather(
-        *[get_info(i, "cache", lock, cache, queue) for i in range(10)]
+        *[get_info(lock) for _ in range(10)]
     )
     return result
 
--- Module_3/test_lock.py
+++ Module_3/test_lock.py
@@ -1,4 +1,4 @@
-import logging
+import time
 
 import pytest
 
@@ -6,7 +6,16 @@ from .lock import main
 
 
 @pytest.mark.asyncio
-async def test_lock(caplog):
-    caplog.at_level(logging.ERROR)
+async def test_lock(mocker):
+    mocker_lock = mocker.MagicMock()
+    with mocker.patch("asyncio.Lock", return_value=mocker_lock):
+        result = await main()
+    assert result == ["data" for _ in range(10)]
+    assert mocker_lock.__aenter__.call_count == 10
+
+
+@pytest.mark.asyncio
+async def test_lock_time():
+    start = time.time()
     await main()
-    assert not caplog.text
+    assert time.time() - start >= 10
