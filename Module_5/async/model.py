358a53b31c9051d5119da4003356fcd71efbdcb2
Anastasia <aftergl0wn@yandex.ru>
2026-01-20 17:49:20 +0400

    Fix remarks

--- Module_5/async/model.py
+++ Module_5/async/model.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from typing import Optional
 
 from sqlalchemy import Integer, String
 from sqlalchemy.ext.asyncio import (
@@ -8,14 +9,14 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine
 )
 from sqlalchemy.orm import (
-    Mapped, declared_attr,
-    declarative_base, mapped_column, sessionmaker
+    DeclarativeBase, Mapped,
+    declared_attr, mapped_column
 )
 
 load_dotenv(".env")
 
 
-class Base:
+class PreBase:
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower()
@@ -23,19 +24,18 @@ class Base:
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
 
 
-Base = declarative_base(cls=Base)
+class Base(PreBase, DeclarativeBase):
+    pass
+
+
 engine = create_async_engine(os.environ["ASYNC_URL"])
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession)
 
 
 class Book(Base):
-    __tablename__ = "book"
-
-    title: Mapped[str] = mapped_column(String(20))
-    year: Mapped[int] = mapped_column(Integer, nullable=True)
+    title: Mapped[str] = mapped_column(String(250))
+    year: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
 
 
 class Author(Base):
-    __tablename__ = "author"
-
-    name: Mapped[str] = mapped_column(String(20))
+    name: Mapped[str] = mapped_column(String(250))
